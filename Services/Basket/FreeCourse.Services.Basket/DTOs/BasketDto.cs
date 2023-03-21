d8926c098e9ce7a970666f53814660fe6f062b9a
yusifizcode <5ba0gh2@code.edu.az>
2023-03-22 02:37:40 +0400

    BasketDto & BasketItemDto created

---
+++ Services/Basket/FreeCourse.Services.Basket/DTOs/BasketDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FreeCourse.Services.Basket.DTOs;
+
+public class BasketDto
+{
+    public string UserId { get; set; }
+    public string DiscountCode { get; set; }
+    public List<BasketItemDto> BasketItems { get; set; }
+    public decimal TotalPrice
+    {
+        get => BasketItems.Sum(x => x.Price * x.Quantity);
+    }
+}
