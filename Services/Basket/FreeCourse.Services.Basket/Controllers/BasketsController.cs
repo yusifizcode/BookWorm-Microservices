03a853721ec6a13c8f7a778380e13f6ee9839c76
yusifizcode <5ba0gh2@code.edu.az>
2023-03-22 03:25:56 +0400

    BasketsController added into Basket microservice

---
+++ Services/Basket/FreeCourse.Services.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,34 @@
+﻿using FreeCourse.Services.Basket.DTOs;
+using FreeCourse.Services.Basket.Services;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Basket.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BasketsController : CustomBaseController
+{
+    private readonly IBasketService _basketService;
+    private readonly ISharedIdentityService _sharedIdentityService;
+
+    public BasketsController(IBasketService basketService, ISharedIdentityService sharedIdentityService)
+    {
+        _basketService = basketService;
+        _sharedIdentityService = sharedIdentityService;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetBasket()
+        => CreateActionResultInstance(await _basketService.GetBasket(_sharedIdentityService.GetUserId));
+
+    [HttpPost]
+    public async Task<IActionResult> SaveOrUpdate(BasketDto basketDto)
+        => CreateActionResultInstance(await _basketService.SaveOrUpdate(basketDto));
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteBasket()
+        => CreateActionResultInstance(await _basketService.Delete(_sharedIdentityService.GetUserId));
+}
