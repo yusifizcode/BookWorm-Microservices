c0a76c22ef47064fd803ae0e9e842ac0f3fd87db
yusifizcode <5ba0gh2@code.edu.az>
2023-03-22 01:54:13 +0400

    Authentication added to PhotoStock microservice with IdentityServer

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -1,11 +1,24 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_photo_stock";
+    options.RequireHttpsMetadata = false;
+});
 
 var app = builder.Build();
 
@@ -17,7 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseStaticFiles();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
