b6e3e5a7c4d87633b9e033d2cd8eccb324371a99
yusifizcode <5ba0gh2@code.edu.az>
2023-03-22 16:15:46 +0400

    Discount entity & IDiscountService added into Discount microservice

---
+++ Services/Discount/FreeCourse.Services.Discount/Services/IDiscountService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Shared.DTOs;
+
+namespace FreeCourse.Services.Discount.Services;
+
+public interface IDiscountService
+{
+    Task<Response<List<Entities.Discount>>> GetAllAsync();
+    Task<Response<Entities.Discount>> GetByIdAsync(int id);
+    Task<Response<NoContent>> Save(Entities.Discount discount);
+    Task<Response<NoContent>> Update(Entities.Discount discount);
+    Task<Response<NoContent>> Delete(int id);
+
+    Task<Response<Entities.Discount>> GetByCodeAndUserId(string code, string userId);
+}
