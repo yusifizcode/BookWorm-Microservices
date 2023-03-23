b01a32bd498899d20e1f276febdcbdfb52582996
yusifizcode <5ba0gh2@code.edu.az>
2023-03-24 00:48:19 +0400

    IIdentityService created in MVC Client

---
+++ Clients/FreeCourse.Web/Models/SignInInput.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models;
+
+public class SignInInput
+{
+    [Display(Name = "Your Email")]
+    public string Email { get; set; }
+    [Display(Name = "Your Password")]
+    public string Password { get; set; }
+    [Display(Name = "Remember me")]
+    public bool Remember { get; set; }
+}
---
+++ Clients/FreeCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Shared.DTOs;
+using FreeCourse.Web.Models;
+using IdentityModel.Client;
+
+namespace FreeCourse.Web.Services.Interfaces;
+
+public interface IIdentityService
+{
+    Task<Response<bool>> SignIn(SignInInput signInInput);
+    Task<TokenResponse> GetAccessTokenByRefreshToken();
+    Task RevokeRefreshToken();
+}
