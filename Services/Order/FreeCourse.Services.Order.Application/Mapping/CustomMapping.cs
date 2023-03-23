23e788da0566b3bfd78d402b4375767ef61d7b41
yusifizcode <5ba0gh2@code.edu.az>
2023-03-23 13:39:11 +0400

    AutoMapper configuration into Application layer in Order microservice

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Order.Application.DTOs;
+using FreeCourse.Services.Order.Domain.OrderAggregate;
+
+namespace FreeCourse.Services.Order.Application.Mapping;
+
+public class CustomMapping : Profile
+{
+    public CustomMapping()
+    {
+        CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+        CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+        CreateMap<Address, AddressDto>().ReverseMap();
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace FreeCourse.Services.Order.Application.Mapping;
+
+public static class ObjectMapper
+{
+    private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CustomMapping>();
+        });
+
+        return config.CreateMapper();
+    });
+
+    public static IMapper Mapper => lazy.Value;
+}
