1a08ea48209de45d59c07aca48dd29e23092f26f
yusifizcode <5ba0gh2@code.edu.az>
2023-03-23 16:48:06 +0400

    PaymentsController added into Payment microservice

---
+++ Services/Payment/FreeCourse.Services.Payment/Controllers/PaymentsController.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Payment.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PaymentsController : CustomBaseController
+{
+    [HttpPost]
+    public IActionResult RecievePayment()
+        => CreateActionResultInstance(Response<NoContent>.Success(200));
+}
