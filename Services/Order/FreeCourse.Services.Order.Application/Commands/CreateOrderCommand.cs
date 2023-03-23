daa2e1394cee47b6bdbddc1577798249c945e3c6
yusifizcode <5ba0gh2@code.edu.az>
2023-03-23 14:34:14 +0400

    MediatR, Command, CommandHandler added into Application layer in Order microservice

---
+++ Services/Order/FreeCourse.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Services.Order.Application.DTOs;
+using FreeCourse.Shared.DTOs;
+using MediatR;
+
+namespace FreeCourse.Services.Order.Application.Commands;
+
+public class CreateOrderCommand : IRequest<Response<CreatedOrderDto>>
+{
+    public string BuyerId { get; set; }
+    public List<OrderItemDto> OrderItems { get; set; }
+    public AddressDto Address { get; set; }
+}
