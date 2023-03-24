bf801548a0ac7900d8ea1e6355337c57980699c2
yusifizcode <5ba0gh2@code.edu.az>
2023-03-25 01:53:07 +0400

    AuthController, SignIn.cshtml (post method) added into MVC client

--- Clients/FreeCourse.Web/Models/SignInInput.cs
+++ Clients/FreeCourse.Web/Models/SignInInput.cs
@@ -4,10 +4,14 @@ namespace FreeCourse.Web.Models;
 
 public class SignInInput
 {
+    [Required]
     [Display(Name = "Your Email")]
     public string Email { get; set; }
+
+    [Required]
     [Display(Name = "Your Password")]
     public string Password { get; set; }
+
     [Display(Name = "Remember me")]
     public bool IsRemember { get; set; }
 }
