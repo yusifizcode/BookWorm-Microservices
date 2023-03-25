2793b5a94ec525f8a9232ef35c5fc03425828b24
yusifizcode <5ba0gh2@code.edu.az>
2023-03-25 16:06:21 +0400

    TokenDelegateHandler added into MVC client

--- Clients/FreeCourse.Web/Program.cs
+++ Clients/FreeCourse.Web/Program.cs
@@ -1,3 +1,4 @@
+using FreeCourse.Web.Handlers;
 using FreeCourse.Web.Models;
 using FreeCourse.Web.Services;
 using FreeCourse.Web.Services.Interfaces;
@@ -18,7 +19,7 @@ var serviceApiSettings = builder.Configuration
 builder.Services.AddHttpClient<IUserService, UserService>(opt =>
 {
     opt.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
-});
+}).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
 
 builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("ClientSettings"));
 builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
