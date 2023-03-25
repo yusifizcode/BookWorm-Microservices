ce34365716a3c780b4d9d7188cab724f537a321f
yusifizcode <5ba0gh2@code.edu.az>
2023-03-26 02:33:21 +0400

    CourseController, Create (get method) added into MVC client

--- Clients/FreeCourse.Web/Models/Catalog/FeatureViewModel.cs
+++ Clients/FreeCourse.Web/Models/Catalog/FeatureViewModel.cs
@@ -1,6 +1,9 @@
-﻿namespace FreeCourse.Web.Models.Catalog;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models.Catalog;
 
 public class FeatureViewModel
 {
+    [Required]
     public int Duration { get; set; }
 }
