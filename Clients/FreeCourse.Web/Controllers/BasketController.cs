e966fd3a22aeaea4ca58a1ba05a593b0a04cf53a
yusifizcode <5ba0gh2@code.edu.az>
2023-03-29 03:26:45 +0400

    BasketController added into MVC client

---
+++ Clients/FreeCourse.Web/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using FreeCourse.Web.Models.Baskets;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Web.Controllers;
+
+public class BasketController : Controller
+{
+    private readonly IBasketService _basketService;
+    private readonly ICatalogService _catalogService;
+
+    public BasketController(IBasketService basketService,
+                            ICatalogService catalogService)
+    {
+        _basketService = basketService;
+        _catalogService = catalogService;
+    }
+
+    public async Task<IActionResult> Index()
+        => View(await _basketService.Get());
+
+    public async Task<IActionResult> AddBasketItem(string courseId)
+    {
+        var course = await _catalogService.GetByCourseId(courseId);
+
+        var basketItemVM = new BasketItemViewModel
+        {
+            CourseId = course.Id,
+            CourseName = course.Name,
+            Price = course.Price,
+        };
+
+        await _basketService.AddBasketItem(basketItemVM);
+
+        return RedirectToAction("Index", "Basket");
+    }
+
+    public async Task<IActionResult> RemoveBasketItem(string courseId)
+    {
+        await _basketService.RemoveBasketItem(courseId);
+        return RedirectToAction("Index", "Basket");
+    }
+}
