c43037e700c341bbcbecd2ca4bfa09f025e6a540
yusifizcode <5ba0gh2@code.edu.az>
2023-03-30 04:20:17 +0400

    OrderService / CreateOrder, GetOrder methods added into MVC client

--- Clients/FreeCourse.Web/Services/OrderService.cs
+++ Clients/FreeCourse.Web/Services/OrderService.cs
@@ -1,18 +1,82 @@
-﻿using FreeCourse.Web.Models.Orders;
+﻿using FreeCourse.Shared.DTOs;
+using FreeCourse.Shared.Services;
+using FreeCourse.Web.Models.Orders;
+using FreeCourse.Web.Models.Payments;
 using FreeCourse.Web.Services.Interfaces;
 
 namespace FreeCourse.Web.Services;
 
 public class OrderService : IOrderService
 {
-    public Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput)
+    private readonly HttpClient _httpClient;
+    private readonly IBasketService _basketService;
+    private readonly IPaymentService _paymentService;
+    private readonly ISharedIdentityService _sharedIdentityService;
+
+    public OrderService(HttpClient httpClient,
+                        IBasketService basketService,
+                        IPaymentService paymentService,
+                        ISharedIdentityService sharedIdentityService)
     {
-        throw new NotImplementedException();
+        _httpClient = httpClient;
+        _basketService = basketService;
+        _paymentService = paymentService;
+        _sharedIdentityService = sharedIdentityService;
     }
 
-    public Task<List<OrderViewModel>> GetOrder()
+    public async Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput)
     {
-        throw new NotImplementedException();
+        var basket = await _basketService.Get();
+        var payment = new PaymentInfoInput()
+        {
+            CardName = checkoutInfoInput.CardName,
+            CardNumber = checkoutInfoInput.CardNumber,
+            Expiration = checkoutInfoInput.Expiration,
+            CVV = checkoutInfoInput.CVV,
+            TotalPrice = basket.TotalPrice,
+        };
+
+        var responsePayment = await _paymentService.RecievePayment(payment);
+        if (!responsePayment)
+            return new OrderCreatedViewModel() { Error = "Payment could not be received!", IsSuccessful = false };
+
+        var orderCreateInput = new OrderCreateInput()
+        {
+            BuyerId = _sharedIdentityService.GetUserId,
+            Address = new AddressCreateInput()
+            {
+                Province = checkoutInfoInput.Province,
+                District = checkoutInfoInput.District,
+                Street = checkoutInfoInput.Street,
+                Line = checkoutInfoInput.Line,
+                ZipCode = checkoutInfoInput.ZipCode
+            },
+        };
+
+        basket.BasketItems.ForEach(x =>
+        {
+            var orderItem = new OrderItemCreateInput()
+            {
+                ProductId = x.CourseId,
+                Price = x.Price,
+                PictureUrl = "",
+                ProductName = x.CourseName
+            };
+            orderCreateInput.OrderItems.Add(orderItem);
+        });
+
+        var response = await _httpClient.PostAsJsonAsync<OrderCreateInput>("orders", orderCreateInput);
+
+        if (!response.IsSuccessStatusCode)
+            return new OrderCreatedViewModel() { Error = "Order could not be created!", IsSuccessful = false };
+
+        return await response.Content.ReadFromJsonAsync<OrderCreatedViewModel>();
+    }
+
+    public async Task<List<OrderViewModel>> GetOrder()
+    {
+        var response = await _httpClient.GetFromJsonAsync<Response<List<OrderViewModel>>>("orders");
+        return response.Data;
     }
 
     public Task SuspendOrder(CheckoutInfoInput checkoutInfoInput)
